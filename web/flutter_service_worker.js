12941c152da743c41d843cc7ed12da6e131a68f0
root <noop@github.com>
2021-12-02 01:05:54 +0000

    Pages 4

--- web/flutter_service_worker.js
+++ web/flutter_service_worker.js
@@ -3,22 +3,22 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "manifest.json": "15f73b7e8a8209c2206210b3ac8dea1b",
-"version.json": "3c1eced887dc9a0a8b116ec95b08e126",
-"icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
+  "version.json": "3c1eced887dc9a0a8b116ec95b08e126",
+"manifest.json": "15f73b7e8a8209c2206210b3ac8dea1b",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
+"icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
+"main.dart.js": "57374c447ba0a886bf6af87ef2e2dd96",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"main.dart.js": "bfb08baafaebee60688120cad734f21a",
-"index.html": "4fa6062e8875f45cf4a0975810e7783d",
-"/": "4fa6062e8875f45cf4a0975810e7783d",
-"assets/NOTICES": "eeb6bad9c2b526553cae50c3a31189f8",
-"assets/AssetManifest.json": "f3b5671025611b194c544ba136520403",
-"assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
+"index.html": "15f3efefca2e573e6d3304508390e91b",
+"/": "15f3efefca2e573e6d3304508390e91b",
 "assets/FontManifest.json": "7b2a36307916a9721811788013e65289",
+"assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
+"assets/AssetManifest.json": "f3b5671025611b194c544ba136520403",
 "assets/assets/rives/liquid_download.riv": "759e9a34b5a0981bad2c4af369491332",
 "assets/assets/rives/little_machine.riv": "193f95c91f3d0c38b27e25ff1b71b6ef",
 "assets/assets/rives/off_road_car.riv": "ed576aa0dda582ecf3a7a311b80415c6",
-"assets/assets/pages/rives.json": "e9bbe60e50bc24fe3721e41a2377d982"
+"assets/assets/pages/rives.json": "e9bbe60e50bc24fe3721e41a2377d982",
+"assets/NOTICES": "eeb6bad9c2b526553cae50c3a31189f8"
 };
 
 // The application shell files that are downloaded before a service worker can
